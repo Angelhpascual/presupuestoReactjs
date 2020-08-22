b38a57012a23948330777c08644f65ea6f3a1bb9
angelhpascual <angelhpascual@gmail.com>
2020-08-22 23:27:06 +0200

    budget react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Pregunta from './components/Pregunta';
+import Formulario from './components/Formulario';
+import Listado from './components/Listado'
+import ControlPresupuesto from './components/ControlPresupuesto';
+
 
 function App() {
+
+  //Definir el state
+  const [presupuesto, guardarPresupuesto] = useState(0);
+  const [restante, guardarRestante] = useState(0);
+  const [mostrarPregunta, actualizarPregunta] = useState(true);
+  const [gastos, guardarGastos] = useState([]);
+
+  //Función que se ejecuta cuando agregamos un nuevo gasto
+  const agregarNuevoGasto = gasto => {
+    guardarGastos([
+      ...gastos,
+      gasto
+    ])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+        <header>
+          <h1>Gasto Semanal</h1>
+          <div className="contenido-principal">
+            {mostrarPregunta ?(
+                <Pregunta
+                guardarPresupuesto={guardarPresupuesto}
+                guardarRestante={guardarRestante}
+                actualizarPregunta={actualizarPregunta}
+              />
+            ) :(
+              <div className="row">
+
+              <div className="one-half column">
+                <Formulario
+                  agregarNuevoGasto={agregarNuevoGasto}
+                />
+              </div>
+              <div className="one-half column">
+                <Listado
+
+                gastos={gastos}
+                
+                />
+
+                <ControlPresupuesto
+                  presupuesto={presupuesto}
+                  restante={restante}
+                />
+                
+              </div>
+
+            </div>
+            )}
+              
+                
+          </div>
+          
+        </header>
+      
     </div>
   );
 }
